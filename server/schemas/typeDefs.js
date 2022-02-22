ecc3ba2cc88540988b1a8976d3aad13a7e43cc48
Mason Newell <mason.newell13@gmail.com>
2022-02-22 14:19:41 -0800

    created input

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,6 +19,15 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+  }
+
   type Auth {
     token: ID!
     user: User
