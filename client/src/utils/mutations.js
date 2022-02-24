da3fda57bf41c347210217ba2c6515c818162d94
Mason Newell <mason.newell13@gmail.com>
2022-02-23 17:11:20 -0800

    created query for User

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,23 @@
 import { gql } from "@apollo/client";
 
-//
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          description
+          title
+          bookId
+          image
+          link
+          authors
+        }
+      }
+      token
+    }
+  }
+`;
