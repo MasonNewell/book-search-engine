9699fa820ccfa0a3400dde2a88de141cb9097a60
Mason Newell <mason.newell13@gmail.com>
2022-02-23 21:30:29 -0800

    login function added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,9 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    user: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
-    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return Profile.findOne({ _id: context.user._id });
+        return User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError("You need to be logged in!");
     },
@@ -20,6 +17,18 @@ const resolvers = {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
 
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invalid Email");
+      }
+      const checkPassword = await user.isCorrectPassword(password);
+      if (!checkPassword) {
+        throw new AuthenticationError("Invalid Password");
+      }
+      const token = signToken(user);
       return { token, user };
     },
   },
