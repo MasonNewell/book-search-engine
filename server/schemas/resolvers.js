8a40f5302c74d3030b6e9c5696a22b7aa236e6af
Mason Newell <mason.newell13@gmail.com>
2022-02-23 16:22:06 -0800

    added me query and addUser  mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,21 @@
-const { Book } = require("../models");
+const { Book, User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async () => {
+      return User.find();
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
