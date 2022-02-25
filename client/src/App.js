445458f23654b93a2b7ef9a0ca9318c08aeb1cd8
Mason Newell <mason.newell13@gmail.com>
2022-02-24 22:39:11 -0800

    added context/token

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,26 @@ import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 // Add Apollo so FE/BE can communicate
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-const client = new ApolloClient({
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
   uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
